425a40b2a0cd977d7029788f8e892a5e102e1159
Luis Vervaet <lrvervae@mtu.edu>
2025-01-17 20:14:31 -0500

    opening via notif works now

--- app/src/main/java/com/larvey/azuracastplayer/mediasession/MusicPlayerService.kt
+++ app/src/main/java/com/larvey/azuracastplayer/mediasession/MusicPlayerService.kt
@@ -1,10 +1,18 @@
 package com.larvey.azuracastplayer.mediasession
 
+import android.Manifest
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.app.PendingIntent
+import android.app.TaskStackBuilder
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import androidx.annotation.OptIn
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import androidx.media3.common.ForwardingPlayer
 import androidx.media3.common.MediaItem
 import androidx.media3.common.util.UnstableApi
@@ -25,7 +33,15 @@ import com.larvey.azuracastplayer.R
 
 
 class MusicPlayerService : MediaSessionService() {
-  private var mediaSession: MediaSession? = null
+
+  private var _mediaSession: MediaSession? = null
+  private val mediaSession get() = _mediaSession!!
+
+  companion object {
+    private const val NOTIFICATION_ID = 123
+    private const val CHANNEL_ID = "session_notification_channel_id"
+    private val immutableFlag = if (Build.VERSION.SDK_INT >= 23) PendingIntent.FLAG_IMMUTABLE else 0
+  }
 
   // Create your player and media session in the onCreate lifecycle event
   @OptIn(UnstableApi::class)
@@ -45,14 +61,17 @@ class MusicPlayerService : MediaSessionService() {
         super.seekToDefaultPosition()
       }
     }
-    mediaSession = MediaSession.Builder(this, player)
+
+
+    _mediaSession = MediaSession.Builder(this, player)
       .setCallback(MyCallback())
       .setCustomLayout(ImmutableList.of(stopButton))
       . also { builder ->
-        getSingleTopActivity()?.let{ builder.setSessionActivity(it) }
+        getSingleTopActivity()?.let { builder.setSessionActivity(it) }
       }
       .build()
 
+    setListener(MediaSessionServiceListener())
 
   }
 
@@ -117,12 +136,17 @@ class MusicPlayerService : MediaSessionService() {
   }
 
   // Remember to release the player and media session in onDestroy
+  @OptIn(UnstableApi::class)
   override fun onDestroy() {
     mediaSession?.run {
+      getBackStackedActivity()?.let{setSessionActivity(it)}
       player.release()
       release()
-      mediaSession = null
+      _mediaSession = null
     }
+
+    clearListener()
+
     super.onDestroy()
   }
 
@@ -131,6 +155,65 @@ class MusicPlayerService : MediaSessionService() {
       applicationContext, 0,
       Intent(applicationContext, MainActivity::class.java),
       PendingIntent.FLAG_IMMUTABLE)
+  }
+
+  private fun getBackStackedActivity(): PendingIntent? {
+    return TaskStackBuilder.create(this).run {
+      addNextIntent(Intent(this@MusicPlayerService, MainActivity::class.java))
+      getPendingIntent(0, immutableFlag or PendingIntent.FLAG_UPDATE_CURRENT)
+    }
+  }
+
+  @OptIn(UnstableApi::class) // MediaSessionService.Listener
+  private inner class MediaSessionServiceListener : Listener {
+
+    /**
+     * This method is only required to be implemented on Android 12 or above when an attempt is made
+     * by a media controller to resume playback when the {@link MediaSessionService} is in the
+     * background.
+     */
+    override fun onForegroundServiceStartNotAllowedException() {
+      if (
+        Build.VERSION.SDK_INT >= 33 &&
+        checkSelfPermission(Manifest.permission.POST_NOTIFICATIONS) !=
+        PackageManager.PERMISSION_GRANTED
+      ) {
+        // Notification permission is required but not granted
+        return
+      }
+      val notificationManagerCompat = NotificationManagerCompat.from(this@MusicPlayerService)
+      ensureNotificationChannel(notificationManagerCompat)
+      val builder =
+        NotificationCompat.Builder(this@MusicPlayerService, CHANNEL_ID)
+          .setSmallIcon(androidx.media3.session.R.drawable.media3_notification_small_icon)
+          .setContentTitle("Playback cannot be resumed")
+          .setStyle(
+            NotificationCompat.BigTextStyle().bigText("Press on the play button on the media notification if it\n" +
+                "    is still present, otherwise please open the app to start the playback and re-connect the session\n" +
+                "    to the controller")
+          )
+          .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+          .setAutoCancel(true)
+          .also { builder -> getBackStackedActivity()?.let { builder.setContentIntent(it) } }
+      notificationManagerCompat.notify(NOTIFICATION_ID, builder.build())
+    }
+  }
+
+  private fun ensureNotificationChannel(notificationManagerCompat: NotificationManagerCompat) {
+    if (
+      Build.VERSION.SDK_INT < 26 ||
+      notificationManagerCompat.getNotificationChannel(CHANNEL_ID) != null
+    ) {
+      return
+    }
+
+    val channel =
+      NotificationChannel(
+        CHANNEL_ID,
+        "Playback cannot be resumed",
+        NotificationManager.IMPORTANCE_DEFAULT
+      )
+    notificationManagerCompat.createNotificationChannel(channel)
   }
 
 
