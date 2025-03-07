f695969670f6ae5a163f0f8d9daec7f319afee91
Luis Vervaet <lrvervae@mtu.edu>
2025-03-07 00:53:36 -0500

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
     applicationId = "com.larvey.azuracastplayer"
     minSdk = 29
     targetSdk = 35
-    versionCode = 55
-    versionName = "beta-1.3"
+    versionCode = 56
+    versionName = "beta-1.4"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
