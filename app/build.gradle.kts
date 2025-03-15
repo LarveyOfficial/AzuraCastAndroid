5a4f05bb0d2596966383505d1c242e76e843ee32
Luis Vervaet <lrvervae@mtu.edu>
2025-03-15 19:50:16 -0400

    Version bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
     applicationId = "com.larvey.azuracastplayer"
     minSdk = 29
     targetSdk = 35
-    versionCode = 56
-    versionName = "beta-1.4"
+    versionCode = 57
+    versionName = "beta-1.5"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
